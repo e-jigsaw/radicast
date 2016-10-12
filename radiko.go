6e7eb0c1c1b9e483fc47e56e178867bdfc4474c3
KASE Masaru <kase@fs>
2016-10-12 20:07:02 +0900

    fix

--- radiko.go
+++ radiko.go
@@ -510,8 +510,8 @@ func (r *Radiko) auth(ctx context.Context) (string, string, error) {
 	}
 
 	req.Header.Set("pragma", "no-cache")
-	req.Header.Set("X-Radiko-App", "pc_1")
-	req.Header.Set("X-Radiko-App-Version", "2.0.1")
+	req.Header.Set("X-Radiko-App", "pc_ts")
+	req.Header.Set("X-Radiko-App-Version", "4.0.0")
 	req.Header.Set("X-Radiko-User", "test-stream")
 	req.Header.Set("X-Radiko-Device", "pc")
 
